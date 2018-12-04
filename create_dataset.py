efdbf828f05aa2510cd94be6c5bfc81f2517f469
cpt-majkel <michalpiekars@gmail.com>
2018-12-05 00:25:57 +0100

    Prepare data to learn

--- create_dataset.py
+++ create_dataset.py
@@ -6,6 +6,8 @@ from os import listdir
 from os.path import isfile, join
 import random
 import pickle
+import numpy as np
+
 
 class CreateDataset:
     """
@@ -15,10 +17,13 @@ class CreateDataset:
                                 example  DC_001.jpg (when it is Daniel Craig )
     """
 
-    def __init__(self, base_dir):
+    def __init__(self, base_dir, do_crop):
         self.base_dir = base_dir
         self.train_dataset = []
         self.test_data = []
+        self.X = []
+        self.y = []
+        self.do_crop = do_crop
         self.initials2name = {
             "SC": "Sean Connery",
             "PB": "Pierce Brosnan",
@@ -27,7 +32,7 @@ class CreateDataset:
             "GL": "George Lazenby",
             "TD": "Timothy Dalton"
         }
-        self.train_face_size = 100 #declaring the size of a single training face for uniformity
+        self.train_face_size = 100  # declaring the size of a single training face for uniformity
 
     def read_test_data(self):
         path = self.base_dir + "/test"
@@ -37,6 +42,7 @@ class CreateDataset:
                 tmp = cv2.imread(path + "/" + bond, cv2.IMREAD_GRAYSCALE)
                 initials = bond.split("_")[0]
                 self.test_data.append((tmp, self.initials2name[str(initials)]))
+            random.shuffle(self.test_data)
         except Exception as e:
             print("Following error occured while reading test data: ", e)
 
@@ -46,9 +52,10 @@ class CreateDataset:
             files = [f for f in listdir(path) if isfile(join(path, f))]
             for bond in files:
                 tmp = cv2.imread(path + "/" + bond, cv2.IMREAD_GRAYSCALE)
-                tmp_path = path + "/" + bond
-                #ret = self.crop_face(tmp, tmp_path)
-                #ret_grayscale = cv2.cvtColor(ret,cv2.COLOR_BGR2GRAY)
+                if self.do_crop:
+                    self.crop_face(tmp, path + "/" + bond)
+                if tmp.shape[0] != self.train_face_size or tmp.shape[1] != self.train_face_size:
+                    tmp = cv2.resize(tmp, (self.train_face_size, self.train_face_size))
                 initials = bond.split("_")[0]
                 self.train_dataset.append((tmp, self.initials2name[str(initials)]))
             random.shuffle(self.train_dataset)
@@ -59,43 +66,29 @@ class CreateDataset:
         self.read_test_data()
         self.read_training_data()
 
-
-    def crop_face(self, path):
+    def crop_face(self, img_gray, path):
         haar = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-        try:
-            files = [f for f in listdir(path) if isfile(join(path, f))]
-            for bond in files:
-                img1 = []
-                print path + "/" + bond
-                tmp = cv2.imread(path + "/" + bond)
-                tmp_path = path + "/" + bond
-                img_gray = cv2.cvtColor(tmp, cv2.COLOR_BGR2GRAY)
-                face = haar.detectMultiScale(img_gray, 1.2)
-                for (x, y, w, h) in face:
-                    # cv2.imwrite(path, img[y:(y + w), x:(x + h)])
-                    img1 = tmp[y:(y + w), x:(x + h)]
-                    self.resize_faces(img1, tmp_path)
-        except Exception as e:
-            print("Following error occured while reading training data: ", e)
+        face = haar.detectMultiScale(img_gray, 1.2)
+        for (x, y, w, h) in face:
+            # cv2.imwrite(path, img[y:(y + w), x:(x + h)])
+            img_gray = img_gray[y:(y + w), x:(x + h)]
 
-    def resize_faces(self, img, path):
-        if img.shape[0] < self.train_face_size or img.shape[1] < self.train_face_size:
-            pass
-        else:
-            img = cv2.resize(img,(self.train_face_size,self.train_face_size))
-            cv2.imwrite(path,cv2.cvtColor(img,cv2.COLOR_BGR2GRAY))
+    def create_model(self):
+        for features, label in self.train_dataset:
+            self.X.append(features)
+            self.y.append(label)
+        self.X = np.array(self.X).reshape(-1, self.train_face_size, self.train_face_size, 1)
 
-    def save_training_to_pickle(self, data, labels): #save training model to pickle
+    def save_training_to_pickle(self):  # save training model to pickle
         pickle_out = open("train_X.pickle", "wb")
-        pickle.dump(data, pickle_out)
+        pickle.dump(self.X, pickle_out)
         pickle_out.close()
         pickle_out = open("train_y.pickle", "wb")
-        pickle.dump(labels, pickle_out)
+        pickle.dump(self.y, pickle_out)
         pickle_out.close()
 
-    def load_training_data_pickle(self): #load pickle training model
-        pickle_data = open("train_X.pickle", "rb")
-        X = pickle.load(pickle_data)
-        pickle_labels = open("train_y.pickle", "rb")
-        y = pickle.load(pickle_labels)
-        return X,y  #returns data and labels, in that order
+    def prepare_training_dataset(self, do_save):
+        self.read_training_data()
+        self.create_model()
+        if do_save:
+            self.save_training_to_pickle()
--- main.py
+++ main.py
@@ -2,20 +2,46 @@
 # AUTHORS: Jalocha, Noga, Piekarski, Tancula
 
 from create_dataset import CreateDataset
-import cv2
+import pickle
+import tensorflow as tf
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
 
 
 def main():
-
     base_image_dir = 'input_data'
-    dataset = CreateDataset(base_image_dir)
-    dataset.read_data()
-    # print(f'Test data: {dataset.test_data}')
-    # print(f'Test data: {dataset.train_dataset}')
-    #dataset.crop_face()
-    cv2.imshow(dataset.train_dataset[5][1], dataset.train_dataset[5][0])
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    dataset = CreateDataset(base_image_dir, False)
+    dataset.prepare_training_dataset(True)
+    X, y = load_training_data_pickle()
+    X = X/255.0
+
+    model = Sequential()
+    model.add(Conv2D(256, (3, 3), input_shape=X.shape[1:]))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(256, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+    model.add(Dense(64))
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+    model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+    model.fit(X, y, batch_size=32, epochs=3, validation_split=0.3)
+
+
+def load_training_data_pickle():  # load pickle training model
+    pickle_data = open("train_X.pickle", "rb")
+    X = pickle.load(pickle_data)
+    pickle_labels = open("train_y.pickle", "rb")
+    y = pickle.load(pickle_labels)
+    return X, y  # returns data and labels, in that order
+
 
 if __name__ == "__main__":
     main()
