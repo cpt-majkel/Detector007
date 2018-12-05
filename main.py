b1bf264b651f519a347e7a6f6306eca72a50bb6e
cpt-majkel <michalpiekars@gmail.com>
2018-12-05 21:17:18 +0100

    Add deep NN training

--- main.py
+++ main.py
@@ -3,36 +3,35 @@
 
 from create_dataset import CreateDataset
 import pickle
-import tensorflow as tf
-from tensorflow.keras.datasets import cifar10
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
-from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 
 def main():
     base_image_dir = 'input_data'
-    dataset = CreateDataset(base_image_dir, False)
-    dataset.prepare_training_dataset(True)
+    #dataset = CreateDataset(base_image_dir, False)
+    #dataset.prepare_training_dataset(True)
     X, y = load_training_data_pickle()
     X = X/255.0
-
+    labelencoder_y_1 = LabelEncoder()
+    y_train = labelencoder_y_1.fit_transform(y)
     model = Sequential()
-    model.add(Conv2D(256, (3, 3), input_shape=X.shape[1:]))
+    model.add(Conv2D(64, (3, 3), input_shape=X.shape[1:]))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(256, (3, 3)))
+    model.add(Conv2D(64, (3, 3)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
     model.add(Dense(64))
-    model.add(Dense(1))
-    model.add(Activation('sigmoid'))
-    model.compile(loss='binary_crossentropy',
+    model.add(Dense(6))
+    model.add(Activation('softmax'))
+    model.compile(loss='sparse_categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
-    model.fit(X, y, batch_size=32, epochs=3, validation_split=0.3)
+    model.fit(X, y_train, batch_size=2, epochs=3, validation_split=0.2)
 
 
 def load_training_data_pickle():  # load pickle training model
