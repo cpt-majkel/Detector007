3c8bdec03899f269d6cf8940a27ea90242afaa11
TNoga0 <t.noga0@gmail.com>
2018-12-22 11:59:13 +0100

    added label printing to image prediction

--- video_test.py
+++ video_test.py
@@ -1,8 +1,12 @@
 import pickle
 import cv2
 import numpy as np
+import tensorflow as tf
+from create_dataset import CreateDataset
 
 def test_video():
+    pickle_classifier = open("classifier_NN.pickle", "rb")
+    classifier = pickle.load(pickle_classifier)
     cap = cv2.VideoCapture('bond2.mp4')
 
     if(cap.isOpened() == False):
@@ -32,15 +36,29 @@ def draw_rectangle_detected(frame):
         cv2.rectangle(frame, (x-30,y-30), (x+w+30,y+h+30), (0,255,0), thickness=3)
         cv2.rectangle(frame,(x-30,y-70),(x+w,y-30),(255,255,0),cv2.FILLED)
         cv2.putText(frame, 'Sample', (x-25, y-40), cv2.FONT_HERSHEY_DUPLEX, 1.0, (0,0,255), lineType=cv2.LINE_AA)
+
 def image_testing():
-    img = cv2.imread('facetest.jpg')
+    names = ["SC","PB","DC","RM","GL","TD"]
+    base_image_dir = 'input_data'
+    dataset = CreateDataset(base_image_dir, False)
+    pickle_classifier = open("classifier_NN.pickle", "rb")
+    classifier = pickle.load(pickle_classifier)
+    img = cv2.imread('facetest1.jpg')
     img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     haar = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
     face = haar.detectMultiScale(img_gray, 1.1)
     for (x, y, w, h) in face:
+        tmp = img_gray[y:(y + w), x:(x + h)]
+        tmp1 = cv2.resize(tmp, (100, 100))
+        input = tmp1[np.newaxis,...,np.newaxis] #adding axes to predict properly
+
+        prediction = classifier.predict(input)
+        which = np.argmax(prediction)
+
+        print(prediction)
         cv2.rectangle(img, (x-30,y-30),(x+w+30,y+h+30), (255, 255, 0), thickness=3)
         cv2.rectangle(img,(x-30,y-70),(x+w,y-30),(255,255,0),cv2.FILLED)
-        cv2.putText(img, 'Sample', (x-25, y-40), cv2.FONT_HERSHEY_DUPLEX, 1.0, (0,0,255), lineType=cv2.LINE_AA)
+        cv2.putText(img, names[which], (x-25, y-40), cv2.FONT_HERSHEY_DUPLEX, 1.0, (0,0,255), lineType=cv2.LINE_AA)
     cv2.imshow('test',img)
     if cv2.waitKey(0) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
